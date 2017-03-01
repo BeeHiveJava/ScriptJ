1012c021a0883c3621f3b7ab18aaea3444ad1949
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2017-03-01 20:48:20 +0100

    Added ScriptInformation to PluginInformation.

--- src/main/java/nl/beehivejava/scriptj/PluginInformation.java
+++ src/main/java/nl/beehivejava/scriptj/PluginInformation.java
@@ -1,5 +1,10 @@
 package nl.beehivejava.scriptj;
 
+import nl.beehivejava.scriptj.script.ScriptInformation;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Objects;
 
 /**
@@ -12,17 +17,29 @@ public final class PluginInformation {
     private final String id;
     private final String author;
 
+    private final Collection<ScriptInformation> scripts = new HashSet<>();
+
     /**
      * Initializes a new {@code PluginInformation} using the given arguments.
      *
-     * @param id     The {@code id} of the {@code Plugin}, should not be {@code null}.
-     * @param author The {@code author} of the {@code Plugin}, should not be {@code null}.
-     * @throws NullPointerException If the given {@code id} is {@code null}.
-     *                              If the given {@code author} is {@code null}.
+     * @param id      The {@code id} of the {@code Plugin}, should not be {@code null}.
+     * @param author  The {@code author} of the {@code Plugin}, should not be {@code null}.
+     * @param scripts The information about the {@code Scripts} that this {@code Plugin} contains,
+     *                should not be {@code null} and should contain at least one {@code Script}.
+     * @throws NullPointerException     If the given {@code id} is {@code null}.
+     *                                  If the given {@code author} is {@code null}.
+     * @throws IllegalArgumentException If there were no {@code Scripts} given.
      */
-    public PluginInformation(String id, String author) {
+    public PluginInformation(String id, String author, ScriptInformation... scripts) {
         this.id = Objects.requireNonNull(id);
         this.author = Objects.requireNonNull(author);
+
+        Objects.requireNonNull(scripts);
+        if (scripts.length < 1) {
+            throw new IllegalArgumentException("At least one script is required.");
+        }
+
+        this.scripts.addAll(Arrays.asList(scripts));
     }
 
     /**
@@ -41,6 +58,15 @@ public final class PluginInformation {
      */
     public String author() {
         return author;
+    }
+
+    /**
+     * Retrieves information about all the {@code Scripts} that this {@code Plugin} contains.
+     *
+     * @return A {@link Collection} of {@code Scripts} that this {@code Plugin} contains.
+     */
+    public Collection<ScriptInformation> scripts() {
+        return scripts;
     }
 
 }
