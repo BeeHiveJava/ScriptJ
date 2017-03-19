24421ca8f25bf33fec6db645ca49ab3addd796f2
Lesley Vente <BeeHiveJava@users.noreply.github.com>
2017-03-19 15:27:18 +0100

    Add a PluginLoader and default tests.

---
+++ src/main/java/nl/beehivejava/scriptj/loader/PluginLoader.java
@@ -0,0 +1,29 @@
+package nl.beehivejava.scriptj.loader;
+
+import nl.beehivejava.scriptj.Plugin;
+import nl.beehivejava.scriptj.PluginInformation;
+import nl.beehivejava.scriptj.script.Script;
+
+import java.io.IOException;
+
+/**
+ * A {@code PluginLoader} can load a {@link Plugin} from an external location, for example, a {@code Plugin} existing
+ * on the filesystem.
+ *
+ * @author Lesley
+ */
+public interface PluginLoader {
+
+    /**
+     * Loads a {@link Plugin} on the given {@code location}. The {@code location} represents
+     * the {@link PluginInformation} file. Note that all {@link Script Scripts} will be searched in the root of the
+     * {@code PluginInformation} file.
+     *
+     * @param location The {@code location} of the {@link PluginInformation} file, should not be {@code null}.
+     * @return The loaded {@code Plugin}.
+     * @throws IOException          If something failed while loading the {@code Plugin} from the given {@code location}.
+     * @throws NullPointerException If the given {@code location} is {@code null}.
+     */
+    Plugin load(String location) throws IOException;
+
+}
---
+++ src/test/java/nl/beehivejava/scriptj/loader/PluginLoaderTest.java
@@ -0,0 +1,26 @@
+package nl.beehivejava.scriptj.loader;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * @author Lesley
+ */
+public abstract class PluginLoaderTest {
+
+    @Rule
+    public final ExpectedException internalExpectedException = ExpectedException.none();
+
+    @Test
+    public final void load_nullLocation_throwsException() throws Exception {
+        internalExpectedException.expect(NullPointerException.class);
+
+        PluginLoader loader = makeDefaultPluginLoader();
+
+        loader.load(null);
+    }
+
+    protected abstract PluginLoader makeDefaultPluginLoader();
+
+}
